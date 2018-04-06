6fa9bfbbe6f074f483c255517f3d7ee602ba8925
Arief Budhiman <ariefbudhiman1997@gmail.com>
2018-04-06 19:41:34 +0700

    6/4/18

--- elbow.py
+++ elbow.py
@@ -4,24 +4,21 @@ from scipy.spatial.distance import cdist
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import time
+df = pd.read_csv('TestsetTugas2.csv', sep='\t', header=0)
+arr = np.array(df)
+# fig = plt.figure(1)
+# ax = fig.add_subplot(1, 1, 1)
 
-file = pd.read_csv('TestsetTugas2.csv', sep='\t', header=0)
-atrain = np.array(file)
-
-fig = plt.figure(1)
-ax = fig.add_subplot(1, 1, 1)
-colors = ['r', 'g', 'b', 'y', 'c', 'm']
-
-x = atrain[:,0]
-y = atrain[:,1]
-
-ax.scatter(x, y)
-plt.show()
-
+x = arr[:,0]
+y = arr[:,1]
+# ax.scatter(x, y)
+# plt.show(block=False)
+# time.sleep(1)
+# plt.close()
 # create new plot and data
 plt.plot()
 X = np.array(list(zip(x, y))).reshape(len(x), 2)
-
 # determine k
 distortions = []
 K = range(1,10)
@@ -29,9 +26,7 @@ for k in K:
     kmeanModel = KMeans(n_clusters=k).fit(X)
     kmeanModel.fit(X)
     distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
-
 # Plot elbow curve
-plt.plot(K, distortions, 'bx-')
-plt.xlabel('')
-plt.title('Optimal K')
+plt.plot(K, distortions, 'g-X')
+plt.title('K for KMeans')
 plt.show()
---
+++ main.py
@@ -0,0 +1,8 @@
+import model
+import elbow
+
+if __name__ == "__main__":
+    print(__name__)
+    y = elbow.elb()
+    x = model.KMeans()
+    x.penentuan()
